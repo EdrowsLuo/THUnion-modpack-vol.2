ed35d128bc18090c378d10a7b0c60cc967d96982
Sigmit64 <sigmadragon@sina.com>
2024-09-14 22:00:49 +0800

    降低了线圈费用，缩短了高炉运行时间

---
+++ kubejs/server_scripts/gtceu/coils.js
@@ -0,0 +1,28 @@
+
+var materials=[
+    'cupronickel','kanthal','nichrome','rtm_alloy','hssg'
+];
+
+ServerEvents.recipes( event => {
+    materials.forEach(material=>{
+      console.log(`gtceu:${material}_coil_block`);
+        event.remove({output: `gtceu:${material}_coil_block`});
+        event.remove({input:`gtceu:${material}_coil_block`,type:'gtceu:macerator'});
+        console.log('123');
+        event.shaped(
+            Item.of(`gtceu:${material}_coil_block`, 1), 
+            [
+              'WWW',
+              'WTW', 
+              'WWW'
+            ],
+            {
+              W: `gtceu:${material}_single_wire`,
+              T: '#forge:tools/wrenches', 
+            }
+          );
+    });
+      
+  }
+
+);
---
+++ kubejs/server_scripts/gtceu/ebf_duration_reduce.js
@@ -0,0 +1,10 @@
+ServerEvents.recipes(event => {
+    event.forEachRecipe({ mod: 'gtceu' , type: 'gtceu:electric_blast_furnace'}, recipe => {
+        try {
+            var newDuration = recipe.get("duration");
+            recipe.set("duration", newDuration/4>=1?newDuration/4:1);
+            
+        } catch (err) {
+        }
+    })
+})
