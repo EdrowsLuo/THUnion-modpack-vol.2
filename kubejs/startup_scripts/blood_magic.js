1ae636014c19f1018b037b550e2e0066b79df7a2
EdrowsLuo <793971297@qq.com>
2024-09-18 23:40:25 +0800

    启用6阶血魔法祭坛

---
+++ kubejs/startup_scripts/blood_magic.js
@@ -0,0 +1,6 @@
+
+// Tier 6 BloodMagic Alter
+ForgeModEvents.onEvent("net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent", event => {
+    const $BloodMagicAPI = Java.loadClass("wayoftime.bloodmagic.impl.BloodMagicAPI")
+    $BloodMagicAPI.INSTANCE.registerAltarComponent(Block.getBlock("cataclysm:void_stone").defaultBlockState(), "CRYSTAL")
+})
