238213eada631014a6414b6aa2eb75309ebb9e90
EdrowsLuo <793971297@qq.com>
2024-09-24 11:47:32 +0800

    修改植物盆配方

--- kubejs/server_scripts/agriculture/botany_pot.js
+++ kubejs/server_scripts/agriculture/botany_pot.js
@@ -1,7 +1,8 @@
 
 
 ServerEvents.recipes(event => {
-
+    _modifyPotRecipe(event)
+    
     /**
      * 
      * @param {OreType} ore 
@@ -47,4 +48,111 @@ ServerEvents.recipes(event => {
     )
 })
 
+/**
+ * 
+ * @param {Internal.RecipesEventJS} event 
+ */
+function _modifyPotRecipe(event) {
+    event.remove({"mod": "botanypots", "type": "crafting_shaped"})
+    event.remove({"mod": "botanypotstiers", "type": "crafting_shaped"})
+    event.remove({"mod": "botanypots", "type": "crafting_shapeless"})
+    event.remove({"mod": "botanypotstiers", "type": "crafting_shapeless"})
+    event.custom({
+      "type": "crafting_shaped",
+      "pattern": [
+        "SHS",
+        "SPS",
+        " S "
+      ],
+      "key": {
+        "S": {
+          "item": "minecraft:terracotta"
+        },
+        "P": {
+          "item": "kubejs:growth_core"
+        },
+        "H": {
+          "item": "minecraft:hopper"
+        }
+      },
+      "result": {
+        "item": "botanypots:terracotta_hopper_botany_pot",
+        "count": 1
+      }
+    })
 
+    event.custom({
+      "type": "crafting_shaped",
+      "pattern": [
+        " E ",
+        "IPI"
+      ],
+      "key": {
+        "P": {
+          "item": "botanypots:terracotta_hopper_botany_pot"
+        },
+        "I": {
+          "item": "ars_nouveau:source_gem_block"
+        },
+        "E": {
+          "item": "ars_nouveau:whirlisprig_shards"
+        }
+      },
+      "result": {
+        "item": "botanypotstiers:elite_terracotta_hopper_botany_pot",
+        "count": 1
+      }
+    })
+
+    event.custom({
+      "type": "crafting_shaped",
+      "pattern": [
+        "HEH",
+        "IPI"
+      ],
+      "key": {
+        "P": {
+          "item": "botanypotstiers:elite_terracotta_hopper_botany_pot"
+        },
+        "I": {
+          "item": "ars_nouveau:ritual_flowering"
+        },
+        "E": {
+          "item": "minecraft:nether_star"
+        },
+        "H": {
+            "item": "ars_nouveau:ritual_harvest"
+        }
+      },
+      "result": {
+        "item": "botanypotstiers:ultra_terracotta_hopper_botany_pot",
+        "count": 1
+      }
+    })
+
+    event.custom({
+      "type": "crafting_shaped",
+      "pattern": [
+        "GEG",
+        "IPI"
+      ],
+      "key": {
+        "P": {
+          "item": "botanypotstiers:ultra_terracotta_hopper_botany_pot"
+        },
+        "I": {
+          "item": "minecraft:netherite_block"
+        },
+        "E": {
+          "item": "minecraft:enchanted_golden_apple"
+        },
+        "G": {
+            "item": "pneumaticcraft:glycerol"
+        }
+      },
+      "result": {
+        "item": "botanypotstiers:creative_terracotta_hopper_botany_pot",
+        "count": 1
+      }
+    })
+}
