7c97d3a73ddb29f92d3987438494de3eaca8af14
EdrowsLuo <793971297@qq.com>
2024-09-25 13:20:02 +0800

    镜像维度怪物属性提升

---
+++ kubejs/server_scripts/dimension.js
@@ -0,0 +1,32 @@
+const DIMENSION_HEALTH_MULTIPLIER = new VariableAttributeModifier(
+    "minecraft:generic.max_health",
+    "DIMENSION_HEALTH_MULTIPLIER",
+    "1f9a1c25-dc6a-49ed-8303-71670d7e6076",
+    "multiply_total")
+
+const DIMENSION_DAMEGE_MULTIPLIER = new VariableAttributeModifier(
+    "minecraft:generic.attack_damage",
+    "DIMENSION_DAMEGE_MULTIPLIER",
+    "d60a24b6-b58a-467a-b6aa-1ae9733b3454",
+    "multiply_total")
+
+EntityEvents.spawned(event => {
+    /** @type {Internal.LivingEntity} */
+    let entity = event.entity
+    if (!entity || !entity.isLiving()) return
+
+    if (event.level.dimension == "overworldmirror:overworld") {
+        let health = 2, damage = 2
+        if(entity.isAnimal()) {
+            health = 10
+        }
+        if(isBoss(entity)) {
+            damage = 4
+            health = 4
+        }
+        DIMENSION_HEALTH_MULTIPLIER.initValue(entity, health)
+        DIMENSION_DAMEGE_MULTIPLIER.initValue(entity, damage)
+        entity.setHealth(entity.getMaxHealth())
+    }
+})
+
