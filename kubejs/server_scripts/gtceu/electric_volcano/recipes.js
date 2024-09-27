3eb9917bd34561b21d56c8ed6cbed18779118334
EdrowsLuo <793971297@qq.com>
2024-09-27 15:23:02 +0800

    添加模拟火山控制器配方

--- kubejs/server_scripts/gtceu/electric_volcano/recipes.js
+++ kubejs/server_scripts/gtceu/electric_volcano/recipes.js
@@ -1,4 +1,17 @@
 ServerEvents.recipes(event=>{
+
+    event.recipes.create.item_application(
+        "gtceu:electrical_volcano",
+        [
+            {
+                "item": "alexscaves:volcanic_core"
+            },
+            {
+                "item": "create:brass_casing"
+            }
+        ]
+    )
+
     event.recipes.gtceu.electric_volcano('lava')
         .circuit(1)
         .duration(100)
