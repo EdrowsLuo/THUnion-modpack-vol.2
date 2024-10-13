2f5e2bd7038bd83230090f5bbcd95f546cb7d1b4
EdrowsLuo <793971297@qq.com>
2024-10-13 20:48:35 +0800

    添加血魔法的石头合成表

---
+++ kubejs/server_scripts/fix/blood_magic_dungeon_stone.js
@@ -0,0 +1,7 @@
+
+ServerEvents.recipes(event => {
+    event.recipes.minecraft.stonecutting("2x bloodmagic:dungeon_brick_slab", "bloodmagic:dungeon_brick1")
+    event.recipes.minecraft.stonecutting("bloodmagic:dungeon_brick1", "bloodmagic:dungeon_stone")
+    event.recipes.minecraft.stonecutting("bloodmagic:dungeon_tile", "bloodmagic:dungeon_stone")
+    event.recipes.minecraft.stonecutting("bloodmagic:dungeon_brick_wall", "bloodmagic:dungeon_stone")
+})
