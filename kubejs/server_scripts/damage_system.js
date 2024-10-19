18e7a19c44743ab3ef04cbe83427f82176327e84
EdrowsLuo <793971297@qq.com>
2024-10-19 09:23:37 +0800

    修改为副手持有骷髅头时会被玩家伤害，并优化部分任务描述。

--- kubejs/server_scripts/damage_system.js
+++ kubejs/server_scripts/damage_system.js
@@ -3,7 +3,9 @@ EntityEvents.hurt(event=>{
     if(event.getSource().getType()=="attributeslib:current_hp_damage"){
         event.cancel()
     }
-    if(event.source.player && event.entity.isPlayer() && event.source.player.offHandItem.id != "minecraft:skeleton_skull") {
-        event.cancel()
+    if(event.source.player && event.entity.isPlayer() && event.player.offHandItem.id != "minecraft:skeleton_skull") {
+        if(event.source.player.UUID != event.entity.UUID) {
+            event.cancel()
+        }
     }
 })
